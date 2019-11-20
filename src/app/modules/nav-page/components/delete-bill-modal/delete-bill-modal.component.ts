8b090e62abeafc569790c812edc7e11d9b58cfd6
Joaquin08 <jgarciag8274@gmail.com>
2019-11-20 00:38:42 -0500

    Cuando se elimina una letra se agrega al historial

--- src/app/modules/nav-page/components/delete-bill-modal/delete-bill-modal.component.ts
+++ src/app/modules/nav-page/components/delete-bill-modal/delete-bill-modal.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from "@angular/core";
 import { SelectedBillService } from "src/app/core/services/bill";
 import { ModalContainerService } from "../../services";
 import { Router } from "@angular/router";
-import { Bill } from "src/app/shared/models";
+import { Bill, Record } from "src/app/shared/models";
 import { BillService } from "src/app/core/http";
 import { BillListService } from "src/app/core/services/bill/bill-list/bill-list.service";
+import { AuthenticationService } from 'src/app/core/authentication';
+import { UserCredentials } from 'src/app/shared/dtos';
+import { RecordService } from 'src/app/core/http/record/record.service';
 
 @Component({
 	selector: "app-delete-bill-modal",
@@ -13,21 +16,45 @@ import { BillListService } from "src/app/core/services/bill/bill-list/bill-list.
 })
 export class DeleteBillModalComponent implements OnInit {
 	public bill: Bill;
+	private currentUser: UserCredentials;
 
 	constructor(
 		private _selectedBillService: SelectedBillService,
 		private _modalContainerService: ModalContainerService,
 		private _router: Router,
-		private _billListService: BillListService
+		private _billListService: BillListService,
+		private _authenticationService: AuthenticationService,
+		private _recordService: RecordService
 	) {
 		this.bill = this._selectedBillService.billValue;
+		this.currentUser = this._authenticationService.currentUserValue;
 	}
 
-	ngOnInit() {}
+	ngOnInit() { }
 
 	public async deleteBill() {
+		const today = new Date();
+
+		let message = "Se elimino una letra por ";
+
+		if (this.bill.type == 1)
+			message += "pagar al ruc " + this.bill.drawerRuc;
+		else message += "cobrar al ruc " + this.bill.draweeRuc;
+
+		message += ", por el monto de " + this.bill.amount;
+
+		if (this.bill.currencyCode == 1) message += " soles.";
+		else message += " dolares.";
+
+		const newRecord: Record = {
+			userId: this.currentUser.id,
+			message: message,
+			date: today
+		};
+
 		try {
 			await this._billListService.deleteById(this.bill.id);
+			await this._recordService.createRecord(newRecord);
 			this.closeModal();
 			this._router.navigate(["home"]);
 		} catch (error) {
